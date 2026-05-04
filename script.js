f8ea01f5781de48275e4be80eff19175fbfae870
Inyama-Mulambya <154053148+Inyama-Mulambya@users.noreply.github.com>
2026-05-04 07:20:45 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+/* script.js */
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.onclick=e=>{
+    const id=a.getAttribute('href');
+    if(id.length>1){
+      e.preventDefault();
+      document.querySelector(id).scrollIntoView({behavior:'smooth'});
+    }
+  }
+});
